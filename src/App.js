cb71515e6db6fd00dcae988e67e39959752d195f
kkpai <paikirank@gmail.com>
2023-05-21 13:01:14 +0100

    src/App.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Github Deployment - Version 2 !!!
+          Github Deployment - Version 3 !!!
         </p>
         <a
           className="App-link"
