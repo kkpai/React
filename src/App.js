471ccd254f93ed4c81d6822fbccfb740968fc5ae
kkpai <kiranpai@kirans-MacBook-Air.local>
2023-05-21 12:50:58 +0100

    src/App.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Github Deployment !!!
+          Github Deployment - Version 2 !!!
         </p>
         <a
           className="App-link"
